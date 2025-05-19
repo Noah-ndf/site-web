f8fbe7969e796fea4ecefd0420dc033bd0f8e6ab
Noah-ndf <noahndiayefalcy@gmail.com>
2025-05-19 17:05:02 +0300

    maj navbar

--- site-psy/frontend/src/components/Navbar.jsx
+++ site-psy/frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useAuth } from '../context/AuthContext';
 import './../styles/Navbar.css';
@@ -33,56 +33,67 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="links">
         <img src="./../../public/pink-logo.png" alt="logo" />
-        <Link to="/">{t('nav.home')}</Link>
-        <Link to="/Presentation">{t('nav.about')}</Link>
-        <Link to="/Tarifs">{t('nav.pricing')}</Link>
-{(!user || user.role === 'client') && (
-  <div className="rdv-dropdown">
-    <button className="rdv-button">
-      {t('nav.appointments')}
-    </button>
-    <ul className="rdv-menu">
-      <li>
-        <Link to={user ? "/prendre-rendez-vous" : "/login"}>
-          {t('nav.makeAppointment')}
-        </Link>
-      </li>
-      {user && (
-        <li>
-          <Link to="/mes-rendez-vous">
-            {t('nav.myAppointments')}
-          </Link>
-        </li>
-      )}
-    </ul>
-  </div>
-)}
+
+        <NavLink to="/" className={({ isActive }) => isActive ? 'active-link' : undefined}>
+          {t('nav.home')}
+        </NavLink>
+        <NavLink to="/Presentation" className={({ isActive }) => isActive ? 'active-link' : undefined}>
+          {t('nav.about')}
+        </NavLink>
+        <NavLink to="/Tarifs" className={({ isActive }) => isActive ? 'active-link' : undefined}>
+          {t('nav.pricing')}
+        </NavLink>
+
+        {/* Client or no user menu */}
+        {(!user || user.role === 'client') && (
+          <div className="rdv-dropdown">
+            <button className="rdv-button">
+              {t('nav.appointments')}
+            </button>
+            <ul className="rdv-menu">
+              <li>
+                <NavLink to={user ? "/prendre-rendez-vous" : "/login"}>
+                  {t('nav.makeAppointment')}
+                </NavLink>
+              </li>
+              {user && (
+                <li>
+                  <NavLink to="/mes-rendez-vous">
+                    {t('nav.myAppointments')}
+                  </NavLink>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+
+        {/* Psychologue menu */}
         {user && user.role === 'psychologue' && (
-  <div className="rdv-dropdown">
-    <button className="rdv-button">
-      {t('nav.appointments')}
-    </button>
-    <ul className="rdv-menu">
-      <li><Link to="/tous-les-rendez-vous">{t('nav.allAppointments')}</Link></li>
-      <li><Link to="/configurer-creneaux">{t('nav.setupSlots')}</Link></li>
-      <li><Link to="/mes-creneaux">{t('nav.mySlots')}</Link></li>
-    </ul>
-  </div>
-)}
+          <div className="rdv-dropdown">
+            <button className="rdv-button">
+              {t('nav.appointments')}
+            </button>
+            <ul className="rdv-menu">
+              <li><NavLink to="/tous-les-rendez-vous">{t('nav.allAppointments')}</NavLink></li>
+              <li><NavLink to="/configurer-creneaux">{t('nav.setupSlots')}</NavLink></li>
+              <li><NavLink to="/mes-creneaux">{t('nav.mySlots')}</NavLink></li>
+            </ul>
+          </div>
+        )}
 
-
+        {/* Language dropdown */}
         <div className="lang-dropdown">
-  <button className="lang-button">
-    {t('nav.language')}
-  </button>
-  <ul className="lang-menu">
-    {languages.map((lang) => (
-      <li key={lang.code} onClick={() => handleLanguageChange(lang.code)}>
-        {lang.label}
-      </li>
-    ))}
-  </ul>
-</div>
+          <button className="lang-button">
+            {t('nav.language')}
+          </button>
+          <ul className="lang-menu">
+            {languages.map((lang) => (
+              <li key={lang.code} onClick={() => handleLanguageChange(lang.code)}>
+                {lang.label}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <div className="navbar-right">
@@ -94,7 +105,7 @@ export default function Navbar() {
             </button>
           </div>
         ) : (
-          <Link className="login" to="/login">{t('nav.login')}</Link>
+          <NavLink className="login" to="/login">{t('nav.login')}</NavLink>
         )}
       </div>
     </nav>
